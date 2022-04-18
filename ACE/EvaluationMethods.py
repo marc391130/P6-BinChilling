09785ef0417f48f7deede278e4c57bf9675aae91
marc391130 <marcusdam2010@hotmail.com>
2022-04-18 10:51:14 +0200

    bug fixes

--- ACE/EvaluationMethods.py
+++ ACE/EvaluationMethods.py
@@ -15,18 +15,20 @@ class ARIEvaluator(Evaluator):
     def evaluate(self, eval_partition: Partition, true_partition: Partition, total_object_amount: int) -> float:
         nij_comp, ni_comp, nj_comp = 0, 0, 0
 
+        first = True
         for eval_cluster in eval_partition.values():
-            first = True
+            if len(eval_cluster) < 2: continue
             ni_comp += self.__binomial_coefficient__(len(eval_cluster))
 
             for true_cluster in true_partition.values():
-                
-                cluster_intersection = eval_cluster.intersection(true_cluster)
-                nij_comp += self.__binomial_coefficient__(len(cluster_intersection))
-
-                if not first: continue
+                if not first or len(true_cluster) < 2: continue
                 nj_comp += self.__binomial_coefficient__(len(true_cluster))
-                first = False
+                
+                cluster_intersection_len = len(eval_cluster.intersection(true_cluster))
+                if cluster_intersection_len < 2: continue
+                nij_comp += self.__binomial_coefficient__(cluster_intersection_len)
+
+            first = False
 
         expected_ri = floor(ni_comp*nj_comp) / self.__binomial_coefficient__(total_object_amount)
         counter = nij_comp - expected_ri
